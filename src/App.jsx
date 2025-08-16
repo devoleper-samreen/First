22a25a3fb57bf7ea3879e0c6b86c83f48bf86799
devoleper-samreen <samreenmalik52292@gmail.com>
2025-08-16 11:49:40 +0530

    add build

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Github Actions</h1>
       <h2>Complete course with 2 projects</h2>
+      <h1>{import.meta.env.VITE_MY_NAME}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
